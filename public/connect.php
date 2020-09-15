bb8034d98b0c1860867b766aac5b8cd1d9d3074a
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-15 15:14:15 -0400

    Fix connection

--- public/connect.php
+++ public/connect.php
@@ -1,9 +1,10 @@
 <?php
 $ini = parse_ini_file('config/app.ini.php');
+$DB_host = $ini['db_host'];
 $DB_name = $ini['db_name'];
 
 try {
-  $connectedDB = new PDO("mysql:host=localhost;dbname=$DB_name", $ini['db_username'], $ini['db_password']);
+  $connectedDB = new PDO("mysql:host=$DB_host;dbname=$DB_name", $ini['db_username'], $ini['db_password']);
   // set the PDO error mode to exception
   $connectedDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
