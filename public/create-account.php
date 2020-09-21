fe1a9d4cba661acf780fc92200e48b2c5b10d90d
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-21 09:06:28 -0400

    Added account creation function (almost completed)
    Also realized this change is a bit too big,
    should've made smaller commits

--- public/create-account.php
+++ public/create-account.php
@@ -5,6 +5,106 @@ ob_start();
 if (!$_SESSION["username"]) {
   include "logout.php";
 }
+
+$passwordClass = 'text-muted';
+
+if (isset($_POST['create_user']) || isset($_SESSION['postdata']['create_user'])) {
+  // define variables and set to empty values
+  $error = [];
+  $username = $password = $confirm_password = '';
+
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $_SESSION['postdata'] = $_POST;
+    $_POST = array();
+    header('Location: ' . $_SERVER['REQUEST_URI'],true,303);
+    exit;
+  } elseif (array_key_exists('postdata', $_SESSION)) {
+    $username = trim($_SESSION['postdata']['username']);
+    $password = trim($_SESSION['postdata']['password']);
+    $confirm_password = trim($_SESSION['postdata']['confirm-password']);
+
+    if (isset($username)) {
+      // check if username only contains letters and numbers
+      if (!preg_match("/^[a-zA-Z\d]*$/",$username)) {
+        $error['username'] = true;
+      } elseif (strlen($username > 255)) {
+        $error['username'] = true;
+      }
+    }
+
+    if (isset($password)) {
+      if (strlen($password) < 8) {
+        $error['password'] = true;
+      } elseif (strlen($password) > 72) {
+        $error['password'] = true;
+      } elseif (!preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%?&*()=+_,\'.\";:{}<>[\]\/\\|\^`~\-]).*$/", $password)) {
+        $error['password'] = true;
+      }
+    }
+
+    if (isset($error['password'])) {
+      $passwordClass = 'text-danger';
+    }
+
+    if (empty($error['password'])) {
+      if ($confirm_password != $password) {
+        $error['confirmPassword'] = 'Les mots de passe ne correspondent pas';
+      }
+    }
+
+    include 'connect.php';
+
+    try {
+      $query_sql = 'SELECT username, email FROM Users WHERE username=:username OR email=:email LIMIT 1';
+      $stmt = $connectedDB->prepare($query_sql);
+      $stmt->execute([
+        ':username' => $username,
+        ':email' => $email
+      ]);
+      $user = $stmt->fetch();
+    } catch(PDOException $e) {
+      echo 'Error: ' . $e->getMessage();
+    }
+
+    if ($user) { // if user exists
+      if (strtolower($user['username']) === strtolower($username)) {
+        $error['generic'] = 'Le nom d\'utilisateur existe déjà';
+      }
+    }
+
+    // DB Insert
+    if (count($error) == 0) {
+      try {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 11]);
+        $insert_sql = 'INSERT INTO Users (username, email, hashed_password)
+        VALUES (:username, :email, :hashed_password)';
+        $stmt = $connectedDB->prepare($insert_sql);
+        $stmt->execute([
+          ':username' => $username,
+          ':email' => $email,
+          ':hashed_password' => $hashed_password
+        ]);
+      } catch(PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+      }
+
+      $to = 'Jakob.Bouchard@outlook.com';
+      $subject = '[KIWOUI AAINTRANET] Nouveau compte';
+      $txt = 'Le compte ' . $username . ' (' . $accountType . ') vient d\'être créé.';
+      $headers = 'From: intranet@jakobbouchard.dev';
+      mail($to,$subject,$txt,$headers);
+
+      $connectedDB = null;
+      $_SESSION['username'] = $username;
+      unset($_SESSION['postdata'], $password, $confirm_password);
+      header('Location: landing-page.php',true,303);
+      exit;
+    }
+
+    unset($_SESSION['postdata'], $password, $confirm_password);
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -90,16 +190,27 @@ if (!$_SESSION["username"]) {
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2">Créer un compte</h1>
         </div>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-          <label class ="sr-only" for="username">Utilisateur</label>
-          <input type="text" class="form-control" id="username" name="username" placeholder="Utilisateur" autocomplete="off" required autofocus>
-          <label class ="sr-only" for="password">Mot de passe</label>
-          <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe" required>
-          <label class ="sr-only" for="password_confirm">Confirmer le mot de passe</label>
-          <input type="password" class="form-control" id="password_confirm" name="password_confirm" placeholder="Confirmer le mot de passe" required>
-          <div class="error-msg text-danger"><?php echo $error["generic"];?>&nbsp;</div>
-          <button type="submit" name="create_user" class="btn btn-lg btn-primary btn-block">Créer le compte</button>
-        </form>
+        <div class="container">
+          <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+            <div class="form-group">
+              <label for="username">Nom d'utilisateur</label>
+              <input type="text" class="form-control" id="username" name="username" aria-describedby="usernameHelp"  required autofocus>
+              <small id="usernameHelp" class="form-text text-muted">Peut seulement contenir des lettres sans accents et des chiffres.</small>
+            </div>
+            <div class="form-group">
+              <label for="password">Mot de passe</label>
+              <input type="password" class="form-control" id="password" name="password" aria-describedby="passwordHelp" required>
+              <small id="passwordHelp" class="form-text <?php echo $passwordClass;?>">Doit contenir : 1 majuscule, 1 minuscule, 1 chiffre, 1 caractère spécial.</small>
+            </div>
+            <div class="form-group">
+              <label for="confirm-password">Confirmer le mot de passe</label>
+              <input type="password" class="form-control" id="confirm-password" name="confirm-password" aria-describedby="confirmPasswordHelp"  required>
+              <small id="confirmPasswordHelp" class="form-text text-danger"><?php echo $error['confirmPassword'];?>&nbsp;</small>
+            </div>
+            <div class="error-msg text-danger"><?php echo $error['generic'];?>&nbsp;</div>
+            <button type="submit" name="create_user" class="btn btn-lg btn-primary btn-block">Créer le compte</button>
+          </form>
+        </div>
       </main>
     </div>
   </div>
@@ -108,5 +219,8 @@ if (!$_SESSION["username"]) {
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   <script src="script/dashboard.js"></script>
+  <script>
+    document.querySelector('#password');
+  </script>
 </body>
 </html>
