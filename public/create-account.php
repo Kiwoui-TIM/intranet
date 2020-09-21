74bd066c427df7840b5510101c1297439b46dc34
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-21 10:10:55 -0400

    Add link to index in header

--- public/create-account.php
+++ public/create-account.php
@@ -146,7 +146,7 @@ if (isset($_POST['create_user']) || isset($_SESSION['postdata']['create_user']))
   <?php } ?>
 
   <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Kiwoui</a>
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="index.php">Kiwoui</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
