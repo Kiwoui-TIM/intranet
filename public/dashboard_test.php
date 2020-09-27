0dafa99747da5cc04e2a8e520c083186cca2e831
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-27 13:43:16 -0400

    Add page title variable in main body

--- public/dashboard_test.php
+++ public/dashboard_test.php
@@ -30,7 +30,7 @@ include( VIEW_NAVIGATION );
 <!-- END INCLUDE NAVIGATION -->
       <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Tableau de bord</h1>
+          <h1 class="h2"><?= $page_title ?></h1>
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group mr-2">
               <button type="button" class="btn btn-sm btn-outline-secondary">Partager</button>
