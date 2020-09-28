7baec30accedfc29c2f64b573229ce67ce24d5d7
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-28 09:02:43 -0400

    Put validation code in login page

--- public/login.php
+++ public/login.php
@@ -1,4 +1,49 @@
-<?php include 'validation.php'; ?>
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+ob_start();
+
+// LOGIN USER
+if (isset($_POST['login_user']) || isset($_SESSION["postdata"]["login_user"])) {
+  // define variables and set to empty values
+  $error = [];
+  $username = $password = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $_SESSION["postdata"] = $_POST;
+    $_POST = array();
+    header('Location: ' . $_SERVER['REQUEST_URI'],true,303);
+    exit;
+  } elseif (array_key_exists('postdata', $_SESSION)) {
+    $username = trim($_SESSION["postdata"]["username"]);
+    $password = trim($_SESSION["postdata"]["password"]);
+
+    include 'connect.php';
+
+    try {
+      $query_sql = "SELECT id, username, hashed_password, team FROM Users WHERE username=:username LIMIT 1";
+      $stmt = $connectedDB->prepare($query_sql);
+      $stmt->execute([':username' => $username]);
+      $user = $stmt->fetch();
+    } catch(PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+
+    if (password_verify($password, $user["hashed_password"])) {
+      session_regenerate_id(true);
+      $_SESSION['id'] = $user['id'];
+      $_SESSION['team'] = $user['team'];
+      $_SESSION['username'] = $username;
+      header('location: index.php');
+      exit;
+    } else {
+      $error["generic"] = "Mauvais utilisateur ou mot de passe";
+    }
+    unset($_SESSION["postdata"], $password);
+  }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
