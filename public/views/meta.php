9b050089e6fc1d0725d0ba574b3db386a8267439
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-28 09:47:43 -0400

    Add favicon everywhere

--- public/views/meta.php
+++ public/views/meta.php
@@ -3,3 +3,4 @@
   <title><?= $page_title ?> - Intranet</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link rel="stylesheet" href="styles/dashboard.css">
+  <link rel="shortcut icon" href="https://via.placeholder.com/72.png/007bff/fff?text=Kiwoui" type="image/png">
