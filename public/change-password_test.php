c6c6f29d633b84a4d22d29f5d30e3fa6d06df7f4
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-28 09:31:49 -0400

    Remove role attribute and add disabled option

--- public/change-password_test.php
+++ public/change-password_test.php
@@ -110,7 +110,7 @@ include( VIEW_HEADER );
 include( VIEW_NAVIGATION );
 ?>
 <!-- END INCLUDE NAVIGATION -->
-      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+      <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2"><?= $page_title ?></h1>
         </div>
@@ -119,6 +119,7 @@ include( VIEW_NAVIGATION );
             <div class="form-group">
               <label for="username">Nom d'utilisateur</label>
               <select class="form-control" name="username" id="username" required>
+                <option value="" disabled>Sélectionner un utilisateur...</option>
 <?php
   include 'connect.php';
   try {
