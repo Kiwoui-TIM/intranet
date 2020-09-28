dbbc3e0872f3a6a37ecc2476aeec7cf1999d80d4
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-28 09:35:11 -0400

    Remove useless toolbar

--- public/index.php
+++ public/index.php
@@ -32,16 +32,6 @@ include( VIEW_NAVIGATION );
       <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2"><?= $page_title ?></h1>
-          <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group mr-2">
-              <button type="button" class="btn btn-sm btn-outline-secondary">Partager</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Exporter</button>
-            </div>
-            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-              <span data-feather="calendar"></span>
-              Cette semaine
-            </button>
-          </div>
         </div>
       </main>
 <!-- START INCLUDE FOOTER -->
