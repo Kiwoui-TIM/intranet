0eb8ea32a31bcf5b666f5bb1292e78e8b1ecd418
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-09-29 11:51:25 -0400

    Add dashboard boilerplate

--- public/index.php
+++ public/index.php
@@ -33,6 +33,179 @@ include( VIEW_NAVIGATION );
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2"><?= $page_title ?></h1>
         </div>
+        <div class="container-fluid">
+<?php
+  include( 'connect.php' );
+  try {
+    $query_sql = 'SELECT account_type FROM Users WHERE username = :username LIMIT 1';
+    $stmt = $connectedDB->prepare($query_sql);
+    $stmt->execute([
+      ':username' => $_SESSION["username"]
+    ]);
+    $user = $stmt->fetch();
+  } catch(PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+  }
+  if ($user['account_type'] == 3) {
+    $sql_query = 'SELECT id, name FROM Projects
+                  WHERE client = :client';
+    $stmt = $connectedDB->prepare($sql_query);
+    $stmt->execute([
+      ':client' => $_SESSION['id']
+    ]);
+  } elseif ($user['account_type'] == 0) {
+    $sql_query = 'SELECT DISTINCT Projects.id, Projects.name FROM Projects
+                  INNER JOIN Milestones ON Projects.id = Milestones.project
+                  ORDER BY Projects.id ASC';
+    $stmt = $connectedDB->prepare($sql_query);
+    $stmt->execute();
+  } else {
+    $sql_query = 'SELECT DISTINCT Projects.id, Projects.name FROM Projects
+                  INNER JOIN Milestones ON Projects.id = Milestones.project
+                  WHERE Milestones.team = :team
+                  ORDER BY Projects.id ASC';
+    $stmt = $connectedDB->prepare($sql_query);
+    $stmt->execute([
+      ':team' => $_SESSION['team']
+    ]);
+  }
+  foreach($stmt as $row) {
+?>
+          <h2><?= $row['name'] ?></h2>
+          <div class="progress mt-3 mb-4" style="height: 20px;">
+            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+          </div>
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>Lorem</td>
+                  <td>ipsum</td>
+                  <td>dolor</td>
+                  <td>sit</td>
+                </tr>
+                <tr>
+                  <td>1,002</td>
+                  <td>amet</td>
+                  <td>consectetur</td>
+                  <td>adipiscing</td>
+                  <td>elit</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>Integer</td>
+                  <td>nec</td>
+                  <td>odio</td>
+                  <td>Praesent</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>libero</td>
+                  <td>Sed</td>
+                  <td>cursus</td>
+                  <td>ante</td>
+                </tr>
+                <tr>
+                  <td>1,004</td>
+                  <td>dapibus</td>
+                  <td>diam</td>
+                  <td>Sed</td>
+                  <td>nisi</td>
+                </tr>
+                <tr>
+                  <td>1,005</td>
+                  <td>Nulla</td>
+                  <td>quis</td>
+                  <td>sem</td>
+                  <td>at</td>
+                </tr>
+                <tr>
+                  <td>1,006</td>
+                  <td>nibh</td>
+                  <td>elementum</td>
+                  <td>imperdiet</td>
+                  <td>Duis</td>
+                </tr>
+                <tr>
+                  <td>1,007</td>
+                  <td>sagittis</td>
+                  <td>ipsum</td>
+                  <td>Praesent</td>
+                  <td>mauris</td>
+                </tr>
+                <tr>
+                  <td>1,008</td>
+                  <td>Fusce</td>
+                  <td>nec</td>
+                  <td>tellus</td>
+                  <td>sed</td>
+                </tr>
+                <tr>
+                  <td>1,009</td>
+                  <td>augue</td>
+                  <td>semper</td>
+                  <td>porta</td>
+                  <td>Mauris</td>
+                </tr>
+                <tr>
+                  <td>1,010</td>
+                  <td>massa</td>
+                  <td>Vestibulum</td>
+                  <td>lacinia</td>
+                  <td>arcu</td>
+                </tr>
+                <tr>
+                  <td>1,011</td>
+                  <td>eget</td>
+                  <td>nulla</td>
+                  <td>Class</td>
+                  <td>aptent</td>
+                </tr>
+                <tr>
+                  <td>1,012</td>
+                  <td>taciti</td>
+                  <td>sociosqu</td>
+                  <td>ad</td>
+                  <td>litora</td>
+                </tr>
+                <tr>
+                  <td>1,013</td>
+                  <td>torquent</td>
+                  <td>per</td>
+                  <td>conubia</td>
+                  <td>nostra</td>
+                </tr>
+                <tr>
+                  <td>1,014</td>
+                  <td>per</td>
+                  <td>inceptos</td>
+                  <td>himenaeos</td>
+                  <td>Curabitur</td>
+                </tr>
+                <tr>
+                  <td>1,015</td>
+                  <td>sodales</td>
+                  <td>ligula</td>
+                  <td>in</td>
+                  <td>libero</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+<?php
+  }
+?>
+        </div>
       </main>
 <!-- START INCLUDE FOOTER -->
 <?php
