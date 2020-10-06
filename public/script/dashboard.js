4626391b618bdbe5e42f0cf125d76c5d791cb84a
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-10-06 11:47:20 -0400

    Add confirmation for projects and milestones

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -5,10 +5,28 @@
 
   feather.replace();
 
-  const delete_btns = document.querySelectorAll('[name="delete_task"]');
-  for (let i = 0; i < delete_btns.length; i++) {
-    delete_btns[i].addEventListener("click", function(e) {
-      let confirmation = confirm('Êtes-vous certain de vouloir supprimer cela ?');
+  const task_delete_btns = document.querySelectorAll('[name="delete_task"]');
+  for (let i = 0; i < task_delete_btns.length; i++) {
+    task_delete_btns[i].addEventListener('click', function(e) {
+      let confirmation = confirm('Êtes-vous certain de vouloir supprimer cette tâche ?');
+      if (!confirmation) {
+        e.preventDefault();
+      }
+    });
+}
+  const milestone_delete_btns = document.querySelectorAll('[name="delete_milestone"]');
+  for (let i = 0; i < milestone_delete_btns.length; i++) {
+    milestone_delete_btns[i].addEventListener('click', function(e) {
+      let confirmation = confirm('Êtes-vous certain de vouloir supprimer ce jalon ?');
+      if (!confirmation) {
+        e.preventDefault();
+      }
+    });
+}
+  const project_delete_btns = document.querySelectorAll('[name="delete_project"]');
+  for (let i = 0; i < project_delete_btns.length; i++) {
+    project_delete_btns[i].addEventListener('click', function(e) {
+      let confirmation = confirm('Êtes-vous certain de vouloir supprimer ce projet ?');
       if (!confirmation) {
         e.preventDefault();
       }
