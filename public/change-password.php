8c0c25ddae37df1f10e12d07c368e99d9d478613
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-10-26 18:09:29 -0400

    Remove page titles

--- public/change-password.php
+++ public/change-password.php
@@ -111,9 +111,6 @@ include( VIEW_NAVIGATION );
 ?>
 <!-- END INCLUDE NAVIGATION -->
       <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2"><?= $page_title ?></h1>
-        </div>
         <div class="container">
           <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
             <div class="form-group">
