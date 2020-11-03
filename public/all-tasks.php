2c86dbe32d6b738690cb35eec8adfd9b9b61de0e
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-11-03 10:46:26 -0500

    Complete refactor to match rest of UI and better organization. Closes #36

--- public/all-tasks.php
+++ public/all-tasks.php
@@ -35,120 +35,216 @@
 <!-- END INCLUDE NAVIGATION -->
       <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
         <div class="container">
+          <div class="card my-4 border-0 shadow">
+            <div class="card-body">
+              <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="GET">
+                <div class="input-group input-group-lg">
+                  <select class="custom-select" name="project" id="project" required>
+                    <option value="" disabled <?= empty($_GET['project']) ? 'selected' : null ?>>Choisir un projet...</option>
 <?php
   include( UTIL_CONNECT );
 
   try {
-    $sql_query = 'SELECT id, username FROM Users
-                  WHERE account_type != 3
-                  ORDER BY id ASC';
-    $stmt = $connectedDB->prepare($sql_query);
-    $stmt->execute();
+    $sql_query = 'SELECT *
+                  FROM   Projects
+                  ORDER  BY id ASC';
+    $projects = $connectedDB->prepare($sql_query);
+    $projects->execute();
   } catch(PDOException $e) {
     echo 'Error: ' . $e->getMessage();
   }
 
-  foreach($stmt as $student_row) {
+  foreach($projects as $project) {
+?>
+                    <option value="<?= htmlspecialchars($project['id']) ?>" <?= $_GET['project'] == $project['id'] ? 'selected' : null ?>>
+                      <?= htmlspecialchars($project['name']) ?>
+                    </option>
+<?php
+  }
+?>
+                  </select>
+                  <div class="input-group-append">
+                    <button class="btn btn-outline-primary" type="submit">Afficher</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+<?php
+  if (!empty($_GET['project'])) {
+    // Project querying
     try {
-      $sql_query = 'SELECT id FROM Tasks
-                    WHERE student = :student';
-      $stmt = $connectedDB->prepare($sql_query);
-      $stmt->execute([
-        ':student' => $student_row['id']
+      $sql_query = 'SELECT id,
+                           name,
+                           completed
+                    FROM   Projects
+                    WHERE  id = :project
+                    ORDER  BY id ASC';
+      $projects = $connectedDB->prepare($sql_query);
+      $projects->execute([
+        ':project' => $_GET['project']
+      ]);
+      $project = $projects->fetch();
+    } catch(PDOException $e) {
+      echo 'Error: ' . $e->getMessage();
+    }
+?>
+          <div class="card my-4 border-0 shadow">
+            <div class="card-header bg-white">
+              <h3 class="h4">
+                <?= htmlspecialchars($project['name']) ?>
+                <?= $project['completed'] == 1 ? '<span class="badge badge-success">Complété</span>' : '<span class="badge badge-danger">Incomplet</span>' ?>
+              </h3>
+            </div>
+            <div class="card-body">
+<?php
+    // Milestone querying
+    $task_exists = false;
+    try {
+      $sql_query = 'SELECT id,
+                           name,
+                           completed
+                    FROM   Milestones
+                    WHERE  project = :project
+                    ORDER  BY id ASC';
+      $milestones = $connectedDB->prepare($sql_query);
+      $milestones->execute([
+        ':project' => $project['id']
       ]);
     } catch(PDOException $e) {
       echo 'Error: ' . $e->getMessage();
     }
 
-    if(!empty($stmt->fetch())) {
+    foreach ($milestones as $milestone) {
 ?>
-          <h2><?= $student_row['username'] ?></h2>
-          <table class="table table-bordered table-hover table-sm">
-            <thead class="thead-dark">
-              <tr class="d-flex">
-                <th class="col-3">Nom</th>
-                <th class="col-3">Jalon</th>
-                <th class="col-3">Projet</th>
-                <th class="col-1">Temps</th>
-                <th class="col-2">Échéance</th>
-              </tr>
-            </thead>
-            <tbody>
+              <div class="m-2 p-3 bg-light rounded shadow-sm">
+                <h4 class="h5 border-bottom border-gray pb-2 mb-0">
+                  <?= htmlspecialchars($milestone['name']) ?>
+                  <?= $milestone['completed'] == 1 ? '<span class="badge badge-success">Complété</span>' : '<span class="badge badge-danger">Incomplet</span>' ?>
+                </h4>
 <?php
-      $sql_query = 'SELECT
-                      Tasks.name AS task_name,
-                      Tasks.due_date,
-                      Tasks.time_spent,
-                      Milestones.name AS milestone_name,
-                      Milestones.project AS milestone_project,
-                      Projects.name AS project_name
-                    FROM Tasks
-                      LEFT JOIN Milestones ON Tasks.milestone = Milestones.id
-                      LEFT JOIN Projects ON Milestones.project = Projects.id
-                    WHERE (Tasks.completed = 0 AND Tasks.student = :student)
-                    ORDER BY Tasks.id ASC';
-      $stmt = $connectedDB->prepare($sql_query);
-      $stmt->execute([
-        ':student' => $student_row['id']
-      ]);
+      try {
+        $sql_query = 'SELECT *
+                      FROM   Users
+                      ORDER  BY id ASC';
+        $students = $connectedDB->prepare($sql_query);
+        $students->execute();
+      } catch(PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+      }
 
-      foreach($stmt as $row) {
-        if ($row['due_date'] < date('Y-m-d')) {
+      foreach ($students as $student) {
+        try {
+          $sql_query = 'SELECT id
+                        FROM   Tasks
+                        WHERE  milestone = :milestone AND student = :student';
+          $tasks = $connectedDB->prepare($sql_query);
+          $tasks->execute([
+            ':milestone' => $milestone['id'],
+            ':student' => $student['id']
+          ]);
+        } catch(PDOException $e) {
+          echo 'Error: ' . $e->getMessage();
+        }
+
+        if(!empty($tasks->fetch())) {
+          $task_exists = true;
 ?>
-              <tr class="d-flex table-danger">
-                <td class="col-3"><?= htmlspecialchars($row['task_name']) ?></td>
-                <td class="col-3"><?= htmlspecialchars($row['milestone_name']) ?></td>
-                <td class="col-3"><?= htmlspecialchars($row['project_name']) ?></td>
-                <td class="col-1"><?= htmlspecialchars($row['time_spent']) ?>h</td>
-                <td class="col-2"><?= htmlspecialchars($row['due_date']) ?></td>
-              </tr>
+                <div class="m-2 p-3 bg-white rounded shadow-sm">
+                  <h5 class="h6 border-bottom border-gray pb-2 mb-0">
+                    <?= htmlspecialchars($student['username']) ?>
+                  </h5>
 <?php
-        } else {
+
+          try {
+            $sql_query = 'SELECT *
+                          FROM   Tasks
+                          WHERE  completed = 0 AND student = :student AND milestone = :milestone
+                          ORDER  BY due_date ASC';
+            $tasks = $connectedDB->prepare($sql_query);
+            $tasks->execute([
+              ':student' => $student['id'],
+              ':milestone' => $milestone['id']
+            ]);
+          } catch(PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+          }
+
+          foreach($tasks as $task) {
+            if ($task['due_date'] < date('Y-m-d')) {
 ?>
-              <tr class="d-flex">
-                <td class="col-3"><?= htmlspecialchars($row['task_name']) ?></td>
-                <td class="col-3"><?= htmlspecialchars($row['milestone_name']) ?></td>
-                <td class="col-3"><?= htmlspecialchars($row['project_name']) ?></td>
-                <td class="col-1"><?= htmlspecialchars($row['time_spent']) ?>h</td>
-                <td class="col-2"><?= htmlspecialchars($row['due_date']) ?></td>
-              </tr>
+                  <div class="media pt-3 border-bottom border-gray">
+                    <div class="media-body pb-3 mb-0 lh-125">
+                      <div class="d-flex justify-content-between align-items-center w-100">
+                        <strong class="text-danger"><?= htmlspecialchars($task['name']) ?></strong>
+                      </div>
+                      <span class="d-block text-danger"><strong><?= htmlspecialchars($task['time_spent']) ?>h</strong> - <?= htmlspecialchars($task['due_date']) ?></span>
+                    </div>
+                  </div>
+<?php
+            } else {
+?>
+                  <div class="media pt-3 border-bottom border-gray">
+                    <div class="media-body pb-3 mb-0 lh-125">
+                      <div class="d-flex justify-content-between align-items-center w-100">
+                        <strong><?= htmlspecialchars($task['name']) ?></strong>
+                      </div>
+                      <span class="d-block"><strong><?= htmlspecialchars($task['time_spent']) ?>h</strong> - <?= htmlspecialchars($task['due_date']) ?></span>
+                    </div>
+                  </div>
+<?php
+            }
+          }
+
+          try {
+            $sql_query = 'SELECT *
+                          FROM   Tasks
+                          WHERE  completed = 1 AND student = :student AND milestone = :milestone
+                          ORDER  BY due_date ASC';
+            $tasks = $connectedDB->prepare($sql_query);
+            $tasks->execute([
+              ':student' => $_SESSION['id'],
+              ':milestone' => $milestone['id']
+            ]);
+          } catch(PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+          }
+
+          foreach($tasks as $task) {
+?>
+                  <div class="media text-muted pt-3 border-bottom border-gray">
+                    <div class="media-body pb-3 mb-0 lh-125">
+                      <div class="d-flex justify-content-between align-items-center w-100">
+                        <strong><del><?= htmlspecialchars($task['name']) ?></del></strong>
+                      </div>
+                      <span class="d-block"><strong><?= htmlspecialchars($task['time_spent']) ?>h</strong> - <?= htmlspecialchars($task['due_date']) ?></span>
+                    </div>
+                  </div>
+<?php
+          }
+?>
+                </div>
+<?php
+        }
+      }
+      if (!$task_exists) {
+?>
+                <h5 class="h6 m-2">
+                  Aucune tâche trouvée
+                </h5>
 <?php
       }
-    }
-    $sql_query = 'SELECT
-                    Tasks.name AS task_name,
-                    Tasks.due_date,
-                    Tasks.time_spent,
-                    Milestones.name AS milestone_name,
-                    Milestones.project AS milestone_project,
-                    Projects.name AS project_name
-                  FROM Tasks
-                    LEFT JOIN Milestones ON Tasks.milestone = Milestones.id
-                    LEFT JOIN Projects ON Milestones.project = Projects.id
-                  WHERE (Tasks.completed = 1 AND Tasks.student = :student)
-                  ORDER BY Tasks.id ASC';
-    $stmt = $connectedDB->prepare($sql_query);
-    $stmt->execute([
-      ':student' => $student_row['id']
-    ]);
-
-    foreach($stmt as $row) {
+      $task_exists = false;
 ?>
-              <tr class="d-flex table-secondary text-muted">
-                <td class="col-3"><?= htmlspecialchars($row['task_name']) ?></td>
-                <td class="col-3"><?= htmlspecialchars($row['milestone_name']) ?></td>
-                <td class="col-3"><?= htmlspecialchars($row['project_name']) ?></td>
-                <td class="col-1"><?= htmlspecialchars($row['time_spent']) ?>h</td>
-                <td class="col-2"><?= htmlspecialchars($row['due_date']) ?></td>
-              </tr>
+              </div>
 <?php
     }
 ?>
-            </tbody>
-          </table>
+            </div>
+          </div>
 <?php
-    }
   }
+
   $connectedDB = null;
 ?>
         </div>
